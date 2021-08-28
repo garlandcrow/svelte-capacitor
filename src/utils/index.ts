3c816561b50790a69195f4d1d5822abc380c7cab
garlandcrow <garlandcrow00@gmail.com>
2021-08-28 17:42:50 +0900

    add date-fns

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,20 @@
-export default 'add you code'
+import { zonedTimeToUtc } from 'date-fns-tz'
+import { format, formatDistance } from 'date-fns'
+
+export const dateFromElixirString = (dateStr: string): Date =>
+  zonedTimeToUtc(new Date(dateStr), 'GMT')
+
+export function formatShortDate(date: Date | string): string {
+  const d = typeof date === 'string' ? dateFromElixirString(date) : date
+  return format(d, 'yyyy-MM-dd')
+}
+
+export function formatLongDate(date: Date | string): string {
+  const d = typeof date === 'string' ? dateFromElixirString(date) : date
+  return format(d, 'yyyy-MM-dd HH:mm:ss')
+}
+
+export function formatRelative(date: Date | string): string {
+  const d = typeof date === 'string' ? dateFromElixirString(date) : date
+  return formatDistance(d, new Date(), { addSuffix: true })
+}
