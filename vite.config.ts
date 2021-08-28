7ed56c031e78a0bf397da596569dc4295fc1ba32
garlandcrow <garlandcrow00@gmail.com>
2021-08-28 16:49:28 +0900

    use default public/dist folder setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import sveltePreprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     // Consult https://github.com/sveltejs/svelte-preprocess for more information
     svelte({
-      preprocess: sveltePreprocess({
-        defaults: {
-          style: 'postcss',
-        },
-        postcss: true,
-      }),
+      preprocess: preprocess(),
     }),
     tsconfigPaths(),
   ],
-  publicDir: './assets/',
-  build: {
-    outDir: './public/',
-  },
-  optimizeDeps: { exclude: ['@roxi/routify', '@urql/svelte'] },
+  optimizeDeps: { exclude: ['@roxi/routify'] },
 })
